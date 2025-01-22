47f379373cc588dbf33d30a1507f5afb3c6cf4d0
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-01-23 00:06:04 +0530

    home

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => {
   return (
     <section class="py-10 bg-dark-50 sm:pt-16 lg:pt-24">
-    <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div class="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
+    <div class="px-13 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div class="grid grid-cols-2 md:col-span-3 lg:grid-cols-2 gap-y-16 gap-x-12">
             <div class="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-                <img class="w-auto h-9" src="https://cdn.rareblocks.xyz/collection/celebration/images/logo.svg" alt="" />
+                <img class="icon" src="https://img.icons8.com/ios/50/lawyer.png" alt="" />
 
                 <p class="text-base leading-relaxed text-gray-600 mt-7">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
 
@@ -54,69 +54,8 @@ const Footer = () => {
                     </li>
                 </ul>
             </div>
-
-            <div>
-                <p class="text-sm font-semibold tracking-widest text-gray-400 uppercase">Company</p>
-
-                <ul class="mt-6 space-y-4">
-                    <li>
-                        <a href="#" title="" class="flex text-base text-gray-400  transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> About </a>
-                    </li>
-
-                    <li>
-                        <a href="#" title="" class="flex text-base  text-gray-400  transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Features </a>
-                    </li>
-
-                    <li>
-                        <a href="#" title="" class="flex text-base  text-gray-400  transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Works </a>
-                    </li>
-
-                    <li>
-                        <a href="#" title="" class="flex text-base  text-gray-400  transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Career </a>
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                <p class="text-sm font-semibold tracking-widest  text-gray-400  uppercase">Help</p>
-
-                <ul class="mt-6 space-y-4">
-                    <li>
-                        <a href="#" title="" class="flex text-base  text-gray-400   transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Customer Support </a>
-                    </li>
-
-                    <li>
-                        <a href="#" title="" class="flex text-base  text-gray-400   transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Delivery Details </a>
-                    </li>
-
-                    <li>
-                        <a href="#" title="" class="flex text-base  text-gray-400  transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Terms & Conditions </a>
-                    </li>
-
-                    <li>
-                        <a href="#" title="" class="flex text-base  text-gray-400  transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Privacy Policy </a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="col-span-2 md:col-span-1 lg:col-span-2 lg:pl-8">
-                <p class="text-sm font-semibold tracking-widest text-gray-400 uppercase">Subscribe to newsletter</p>
-
-                <form action="#" method="POST" class="mt-6">
-                    <div>
-                        <label for="email" class="sr-only">Email</label>
-                        <input type="email" name="email" id="email" placeholder="Enter your email" class="block w-full p-4 text-gray-400  placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
-                    </div>
-
-                    <button type="submit" class="inline-flex items-center justify-center px-2 py-2 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700">Subscribe</button>
-                </form>
-            </div>
-        </div>
-
-        <hr class="mt-16 mb-10 border-gray-200" />
-
-        <p class="text-sm text-center text-gray-600">© Copyright 2021, All Rights Reserved by Postcraft</p>
-    </div>
+          </div>
+          </div>
 </section>
 
   )
